05b925d26a0dc8d77e08f566a0acf36972cc1b19
Igor Durbek <igor@igordurbek.com>
2025-07-02 19:03:40 +0200

    Reset button

--- shiny-svelte/src/lib/path.svelte.ts
+++ shiny-svelte/src/lib/path.svelte.ts
@@ -1,7 +1,8 @@
 export const pathState = $state({
 	backendBase: 'http://localhost:8080/',
 	backendDoubleGold: 'http://localhost:8080/double-gold',
-	backendSemaphoreColor: 'http://localhost:8080/get-semaphore-color',
+	backendSemaphoreColor: 'http://localhost:8080/semaphore-color',
 	backendBetRed: 'http://localhost:8080/bet-red',
-	backendBetGreen: 'http://localhost:8080/bet-green'
+	backendBetGreen: 'http://localhost:8080/bet-green',
+	backendReset: 'http://localhost:8080/reset'
 });
