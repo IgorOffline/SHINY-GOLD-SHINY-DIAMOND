a4ccccac8ee3ac913d71f7e4845415257fa06017
IgorOffline <73474119+IgorOffline@users.noreply.github.com>
2025-07-05 21:52:06 +0200

    Bet red/green redesign (#6)

--- ShinySpring/src/main/java/igoroffline/shiny/shinyspring/main/Magic.java
+++ ShinySpring/src/main/java/igoroffline/shiny/shinyspring/main/Magic.java
@@ -3,6 +3,7 @@ package igoroffline.shiny.shinyspring.main;
 public class Magic {
 
     public static final int DEFAULT_GOLD_AMOUNT = 50;
+    public static final int BET_LOSS_GOLD_AMOUNT = 100;
     public static final int LOWEST_POSSIBLE_GOLD_AMOUNT = 0;
     public static final int INVALID_GOLD_AMOUNT = -1;
     public static final String NOT_ENOUGH_GOLD = "not-enough-gold";
